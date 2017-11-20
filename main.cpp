32ef2b92c61b7a3ccaf0b32b7633280c90f8e5b7
izamorsantana <IZAMORSANTANA>
2017-11-20 20:07:44 -0300

    a

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <iostream>
+#include  <vector> 
+using std:: cout;
+using std:: cin;
+using std:: vector;
+
+int main(int argc, char **argv)
+{
+    vector <int> vt (10);
+    for(int i=0; i< vt.size(); i++)
+    {
+          cout <<"Entre com o valor do ">>;
+    }
+    
+
+  cin >> vector;
+       	
+	return 0;
+}
